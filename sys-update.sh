9ea609246da19e6c538b7aa1ca7346f0b88d1d34
Zach Tatlock <ztatlock@cs.washington.edu>
2016-12-15 10:13:08 -0800

    check if repo already cloned

--- sys-update.sh
+++ sys-update.sh
@@ -7,30 +7,43 @@ SENSOR_NET="$HOME/sensor-net"
 
 mkdir -p "$SENSOR_NET"
 pushd "$SENSOR_NET"
-	# TODO see if repos are already cloned, in which case just pull
-
-  git clone 'https://github.com/uwplse/StructTact.git'
+  if [ ! -d StructTact ]; then
+    git clone 'https://github.com/uwplse/StructTact.git'
+  fi
   pushd StructTact
+    git pull
     ./build.sh
   popd
 
-  git clone 'https://github.com/DistributedComponents/InfSeqExt.git'
+  if [ ! -d InfSeqExt ]; then
+    git clone 'https://github.com/DistributedComponents/InfSeqExt.git'
+  fi
   pushd InfSeqExt
+    git pull
     ./build.sh
   popd
 
-  git clone -b fair-shim-fds 'https://github.com/uwplse/verdi.git'
+  if [ ! -d verdi ]; then
+    git clone -b fair-shim-fds 'https://github.com/uwplse/verdi.git'
+  fi
   pushd verdi
+    git pull
     ./build.sh
   popd
 
-  git clone -b v8.5 https://github.com/coq-contribs/aac-tactics.git AAC_tactics
+  if [ ! -d AAC_tactics ]; then
+    git clone -b v8.5 https://github.com/coq-contribs/aac-tactics.git AAC_tactics
+  fi
   pushd AAC_tactics
+    git pull
     make
   popd
 
-  git clone git@github.com:DistributedComponents/verdi-aggregation.git
+  if [ ! -d verdi-aggregation ]; then
+    git clone git@github.com:DistributedComponents/verdi-aggregation.git
+  fi
   pushd verdi-aggregation
+    git pull
     ./build.sh
   popd
 popd
