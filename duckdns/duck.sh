3a6141ae73f34ff70254fdab2f68796004664d05
Zach Tatlock <ztatlock@cs.washington.edu>
2016-12-28 21:05:28 -0800

    timestamp dns logs

--- duckdns/duck.sh
+++ duckdns/duck.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 DBR="$HOME/discoberry-farm"
+LOG="$DBR/duckdns/duck.log"
 
 D="$(hostname)"
 T="$(cat "$DBR/DNSTOK")"
 A="$($DBR/ip.sh)"
 
 echo url="https://www.duckdns.org/update?domains=$D&token=$T&ip=$A&verbose=true" \
-  | curl -k -o "$DBR/duckdns/duck.log" -K -
+  | curl -k -o "$LOG" -K -
+echo >> "$LOG"
+date >> "$LOG"
