c44f1d730bc5f55e97c7256f97bb0506efa35a35
Zach Tatlock <ztatlock@cs.washington.edu>
2016-12-28 20:56:26 -0800

    more deps

--- apt-update.sh
+++ apt-update.sh
@@ -3,5 +3,5 @@ apt-get -y upgrade
 apt-get -y dist-upgrade
 apt-get -y autoremove
 apt-get -y install \
-  vim curl openssh-server mosh tmux \
+  vim curl openssh-server lsof screen mosh tmux \
   m4 mercurial darcs build-essential opam
